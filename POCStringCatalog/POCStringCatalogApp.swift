854d886ba01677c17085b3b3703d1b9d4d3133bc
Herve Peroteau <herve.peroteau@icloud.com>
2023-08-05 14:13:51 +0200

    Initial Commit

---
+++ POCStringCatalog/POCStringCatalogApp.swift
@@ -0,0 +1,17 @@
+//
+//  POCStringCatalogApp.swift
+//  POCStringCatalog
+//
+//  Created by Herve Peroteau on 05/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct POCStringCatalogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
